e50fa411a5a233dab4fe52fb715c482851137b46
강민혁 <rkdglqkr12@naver.com>
2025-06-23 09:58:53 +0900

    refactor: 리팩터링 완료

--- src/main/java/roomescape/core/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/core/exception/GlobalExceptionHandler.java
@@ -1,10 +1,8 @@
 package roomescape.core.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
