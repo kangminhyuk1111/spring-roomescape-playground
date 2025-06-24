d49a8f2cd098f56a8a30d7cfb3fec384c12e374c
강민혁 <rkdglqkr12@naver.com>
2025-06-24 10:26:32 +0900

    fix: 5단계 테스트 코드 수정 - getCatalog

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -102,7 +102,7 @@ public class MissionStepTest {
     void 오단계() {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             assertThat(connection).isNotNull();
-            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getCatalog()).isEqualTo("ROOMESCAPE");
             assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
         } catch (SQLException e) {
             throw new RuntimeException(e);
